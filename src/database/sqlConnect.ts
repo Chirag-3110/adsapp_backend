5c3fb0ca4f6842ce5443f403ed38789adec1db2d
Chirag-3110 <takc206@gmail.com>
2025-09-23 22:46:52 +0530

    appi docs

--- src/database/sqlConnect.ts
+++ src/database/sqlConnect.ts
@@ -5,7 +5,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || 'localhost',
+  host: '127.0.0.1',
   user: process.env.DB_USER || 'app_db',
   password: process.env.DB_PASS || 'adsapp',
   database: process.env.DB_NAME || 'adsapp',
