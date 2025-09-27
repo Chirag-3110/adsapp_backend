bbec7675b882e9ed119c59ca593fcf23bdabdf00
Chirag-3110 <takc206@gmail.com>
2025-09-28 00:31:52 +0530

    some

--- src/middleware/uploadMiddleware.ts
+++ src/middleware/uploadMiddleware.ts
@@ -2,7 +2,8 @@ import multer from 'multer';
 import path from 'path';
 import fs from 'fs';
 
-const uploadPath = path.join(__dirname, '../uploads/profileImages');
+// const uploadPath = path.join(__dirname, '../uploads/profileImages');
+const uploadPath = path.join(process.cwd(), 'uploads', 'profileImages');
 if (!fs.existsSync(uploadPath)) {
   fs.mkdirSync(uploadPath, { recursive: true });
 }
